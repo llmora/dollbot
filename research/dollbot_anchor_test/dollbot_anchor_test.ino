cf50b06f366ff6d5c7073fbc394f783c25d404e2
Lluis Mora <lluismh@gmail.com>
2016-05-08 16:27:45 +0200

    Fixed speed mapping code, it was causing values out of range to be set. Added automatic disabling of manual control if RC is not connected

--- research/dollbot_anchor_test/dollbot_anchor_test.ino
+++ research/dollbot_anchor_test/dollbot_anchor_test.ino
@@ -17,8 +17,8 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_HMC5883_U.h>
 
-#undef DEBUG
-//#define DEBUG
+#define DEBUG // Define if you are debugging the project - note that a huge amount of serial debugging plus a 500ms delay is added to each loop to be able to follow serial output, so debugging heavily interferes with any real-time operation.
+#define DEBUG_DRYDOCK // Define if you are doing workbench testing with no need to have actual motor movement. Generally you do not want the water-designed motors to be spinning out of water for a long period of time.
 
 #define PIN_CH1 7
 #define PIN_CH2 6
@@ -30,8 +30,9 @@
 #define MOTOR_BL 3
 #define MOTOR_BR 4
 
+// These two values need to be adjusted depending on your RC receiver.
 #define PWM_LOW 1000
-#define PWM_HIGH 2000
+#define PWM_HIGH 1900
 
 #define RC_THRESHOLD 10 // Minimum value of an RC input to move to manual control
 #define MOTOR_THRESHOLD 5 // Minimum value of a motor input to spin, smaller than this is considered noise
@@ -65,48 +66,65 @@ Adafruit_HMC5883_Unified compass = Adafruit_HMC5883_Unified(8431);
 void setup() {
   // Initialize serial communications
   Serial.begin(9600);
+  Serial.println("");
+  Serial.print("DOLLBOT v1.0 (");
+
+#ifdef DEBUG_DRYDOCK
+  Serial.print("DRYDOCK ");
+#endif
+
+#ifdef DEBUG
+  Serial.print("DEBUG");
+#else
+  Serial.print("PRODUCTION");
+#endif
+
+  Serial.print(") initialising");
+
+  // Seep for 3 seconds
+  for(int i=0; i < 3; i++) {
+    delay(1000);
+    Serial.print(".");
+  }
   
-  Serial.println("Initializing input...");
+  Serial.println("");
 
   AFMS.begin();  // create with the default frequency 1.6KHz
 
   /* Initialise the compass */
-  if(!compass.begin())
-  {
+  if(!compass.begin()) {
     Serial.println("HMC5883 compass not detected, stopping.");
     while(1);
   }
+  
+  Serial.println("HMC5883 compass initialised");
 
   // Initialize the ports where we will read receiver input
   pinMode(PIN_CH1, INPUT);
   pinMode(PIN_CH2, INPUT);
   pinMode(PIN_CH3, INPUT);
   pinMode(PIN_CH4, INPUT);
-  
-  // Automated control initialisation
-  manualControl = 1;
-  
-#ifdef DEBUG
-  Serial.println("DEBUG mode enabled");
-#endif
 
-  Serial.println("Initialization completed");
+  // Automated control initialisation
+  manualControl = 1; 
+
+  Serial.println("DOLLBOT Initialisation completed");
 }
 
 void dumpContent(int mm[4]) {
-  Serial.print("MM values: ");
+  Serial.print("MOTOR: Input array: ");
   for(int i=0; i < 4; i++) {
     Serial.print(mm[i]);
     Serial.print(",");
   }
-  Serial.println("");
+  Serial.print(". ");
 }
 
 void HolonomicSpin(int mm[4]) {
-  HolonomicSimplify(&mm);
-  
+  HolonomicSimplify(mm);
+
 #ifdef DEBUG
-  dumpContent(mm);
+  Serial.print("MOTOR: Setting speeds: ");
 #endif
 
   for(int i = 0; i < 4; i++) {
@@ -115,30 +133,42 @@ void HolonomicSpin(int mm[4]) {
     mm[i] = map(mm[i], -100, 100, -1 * MOTOR_MAX, MOTOR_MAX);
     
     if(abs(mm[i]) > (MOTOR_THRESHOLD * MOTOR_MAX) / 100) {
+
+#ifndef DEBUG_DRYDOCK
       motor[i]->setSpeed(abs(mm[i]));
       motor[i]->run(mm[i] > 0? FORWARD : BACKWARD);
+#endif
+
 #ifdef DEBUG
-      Serial.print("Spinning motor #");
-      Serial.print(i);
-      Serial.print(": ");
-      Serial.println(mm[i]);
+      Serial.print(mm[i]);
 #endif
     } else {
+
+#ifndef DEBUG_DRYDOCK
       motor[i]->setSpeed(0);
       motor[i]->run(RELEASE);
+#endif
+
 #ifdef DEBUG
-      Serial.print("Stopping motor #");
-      Serial.println(i);
+      Serial.print("STP");
 #endif
     }
+
+#ifdef DEBUG
+    if(i < 3) {
+      Serial.print(",");
+    } else {
+      Serial.println("");
+    }
+#endif
   }
 }
 
-void HolonomicSimplify(int **mm) {
+void HolonomicSimplify(int mm[4]) {
   int max = 0;
 
   for(int i = 0; i < 4; i++) {
-    int currentValue = abs(*mm[i]);
+    int currentValue = abs(mm[i]);
     
     if(currentValue > max) {
       max = currentValue;
@@ -149,7 +179,7 @@ void HolonomicSimplify(int **mm) {
 
   if(max > 100) {
     for(int i = 0; i < 4; i++) {
-      *mm[i] = map(*mm[i], -1 * max, max, -100, 100);
+      mm[i] = map(mm[i], -1 * max, max, -100, 100);
     }
   }
 
@@ -208,19 +238,19 @@ void loop() {
   int channelHorizontal = pulseIn(PIN_CH4, HIGH, 25000); // X axis movement
   int channelVertical = pulseIn(PIN_CH2, HIGH, 25000); // Y axis movement
   int channelRotate = pulseIn(PIN_CH1, HIGH, 25000); // Rotation
-
+    
   int directionHorizontal = map(channelHorizontal, PWM_LOW, PWM_HIGH, -100, 100);
   int directionVertical = map(channelVertical, PWM_LOW, PWM_HIGH, -100, 100);
   int angleRotate = map(channelRotate, PWM_LOW, PWM_HIGH, -100, 100);
   
   // If we are receiving control commands from the RC controller bypass autonomous mode
 
-  if(abs(directionHorizontal) > RC_THRESHOLD || abs(directionVertical) > RC_THRESHOLD || abs(angleRotate) > RC_THRESHOLD) {
+  if((channelHorizontal || channelVertical || channelRotate) && // If we are receiving no pulses it means we are not connected to RC, so ignore manual control
+     (abs(directionHorizontal) > RC_THRESHOLD || abs(directionVertical) > RC_THRESHOLD || abs(angleRotate) > RC_THRESHOLD)) { // RC signals are not perfect, we allow for a 10% threshold before switching to it
     
     manualControl = 1;
     
     // Map all readings to a -100 .. +100 range so it is easy to deal with
-
 
 #ifdef DEBUG    
     Serial.print("MANUAL: RC control ");
@@ -277,8 +307,7 @@ void loop() {
   }
 
 #ifdef DEBUG  
- Serial.println("Debug sleeping");
- delay(500);
+ delay(1000);
 #endif
 }
 
